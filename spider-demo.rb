601e2b0535f270dc8813ef47eb9e4421029ec247
Liu Chong <liuchong@mail.com>
2019-04-24 12:45:59 +0800

    Update spider script

--- spider-demo.rb
+++ spider-demo.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 
 # 防止屏蔽，用于假冒的 User-Agent 的数组
 USER_AGENTS = [
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
 ]
 
