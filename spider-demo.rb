925de42a43d950d17abc24b3637f06ab28b1281b
Liu Chong <liuchong@mail.com>
2019-04-27 06:28:30 +0800

    Update big pic

--- spider-demo.rb
+++ spider-demo.rb
@@ -20,6 +20,7 @@ DIR = File.dirname(File.absolute_path(__FILE__))
 FileUtils.mkpath File.join(DIR, "images")
 
 
+# 去亚马逊获取网页
 def amazon_get(path, args)
   Net::HTTP.start("www.amazon.com", use_ssl: true) { |http|
     http.verify_mode = 0
@@ -30,27 +31,29 @@ def amazon_get(path, args)
   }
 end
 
+# 迭代处理每一个关键字
 KEYWORKS.each { |kw|
-  res = amazon_get("/s", { k: kw })
-  doc = Nokogiri::HTML(res.body)
-  page_count = doc.css("ul.a-pagination li.a-disabled").last
-  page_count_num = page_count.text.to_i if page_count
+  res = amazon_get("/s", { k: kw })  # 去亚马逊查询关键字
+  doc = Nokogiri::HTML(res.body)     # 解析网页
+  page_count = doc.css("ul.a-pagination li.a-disabled").last  # 获取网页中的页码元素
+  page_count_num = page_count.text.to_i if page_count         # 获取页码数字的值
 
-  docs = [ doc ]
+  docs = [ doc ]   # 页面数组，因为有多个翻页
 
-  (2..page_count_num).each { |page|
-    res = amazon_get("/s", { k: kw, page: page })
-    docs << Nokogiri::HTML(res.body)
+  (2..page_count_num).each { |page|                     # 获取多个翻页页面
+    res = amazon_get("/s", { k: kw, page: page })       # 去亚马逊查询对应的关键字的对应的页面
+    docs << Nokogiri::HTML(res.body)                    # 把页面扔进页面数组
   } if page_count_num
 
-  docs.each { |doc|
-    p doc.css('img.s-image').each { |img|
-      img_src = img.attribute("src")
-      p img_src_value = img_src.value if img_src
-      filename = File.basename img_src_value
-      file = File.join(DIR, "images", filename)
-      next if File.exists?(file)
-      open(file, "wb") { |fo| fo.write Net::HTTP.get URI img_src_value }
+  docs.each { |doc|                                     # 遍历页面数组，查询图片
+    p doc.css('img.s-image').each { |img|               # 遍历页面中的商品图片，并且打印出每页的图片数量
+      img_src = img.attribute("src")                    # 获取图片的 src 属性
+      img_src_value = img_src.value if img_src          # 获取图片 src 属性的值
+      p img_src_value = img_src_value.sub(/UL\d+_.jpg/, "UL960_.jpg") if img_src_value    # 替换小图片为大图片
+      filename = File.basename img_src_value                                              # 确定图片文件名称，用于下载
+      file = File.join(DIR, "images", filename)                                           # 确定图片下载路径
+      next if File.exists?(file)                       # 如果图片已经下载过了，就跳过
+      open(file, "wb") { |fo| fo.write Net::HTTP.get URI img_src_value }    # 下载并且写入文件
     }.size
   }
 }
